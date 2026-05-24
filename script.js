40a014eb70e4c1933b5c5eced167749b84bef01c
Shrikant Vibhute <151454749+Shrikantvibhute2003@users.noreply.github.com>
2026-05-24 13:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let index = 0; index < bound; index++) {
+		if(str.indexOf(str[i])==str.lastIndexOf[str[i]]){
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
